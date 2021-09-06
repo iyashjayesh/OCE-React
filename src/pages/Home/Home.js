d25e5ba408fcd5d19564559ca58adb6e29487ebf
iyashjayesh <180303105046@paruluniversity.ac.in>
2021-09-07 03:23:52 +0530

    File Structure Ready

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home container">
+            <h1>Yash</h1>
+            <div className="row">
+                <div className="col text-content">
+                    <h1>Test 1</h1>
+                </div>
+                <div className="col">
+                    <h1>test 2</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home

c6dcbcf295ae5869454e9a0ede1d264b9b5c29ed
Yash Chauhan <ychauhan@altair.com>
2021-09-07 18:31:04 +0530

    Error Resolve

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Nav, Navbar, NavDropdown, Container} from 'react-bootstrap'
+import { Nav, Navbar, Container} from 'react-bootstrap'
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home">Online Code Editor - HTML,CSS,JS</Navbar.Brand>
+                <Navbar.Brand href="/">Online Code Editor - HTML,CSS,JS</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
                     {/* <Nav className="me-auto">
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,29 @@
+/**
+FILE NAME: Home.js
+DESCRIPTION: Simple home page with bootstrap banner and header component 
+
+DATE MODIFIED ON: <07-09-2021>
+VERSION: v1
+AUTHOR: <Yash Chauhan, iyashjayesh@gmail.com>
+Last Updated By: <Yash Chauhan, iyashjayesh@gmail.com>
+**/
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Main from '../../asset/img/main.gif'
-import Header from '../../components/Header/Header'
 
 const Home = () => {
     return (
         <>
-<<<<<<< HEAD
             <div className="home container">
-=======
-            <Header/>
-            <div className="home container">
-                {/* <h1>Yash</h1> */}
->>>>>>> yash-new
                 <div className="row">
                     <div className="col text-content">
                         <h1>Online Code Editor</h1>
                         <span>A Simple Online Code Editor <br/>Just like Code Pen!</span>
                         <br/>
-<<<<<<< HEAD
                         <Link to="/online-code-editor">
                             <button type="button" className="btn btn-primary mt-4 bon">Try it!</button>
                         </Link>
-=======
-                        <button type="button" className="btn btn-primary mt-4">Try it!</button>
->>>>>>> yash-new
                     </div>
                     <div className="col img-section">
                         <img className="img-size" src={Main} alt="main-img"/>

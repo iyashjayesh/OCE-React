e0c0edc91604f9c58c8f32f2dd3be1a55f4d5ea2
iyashjayesh <180303105046@paruluniversity.ac.in>
2021-09-07 14:08:58 +0530

    Changes made

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import Main from '../../asset/img/main.gif'
 
 const Home = () => {
     return (
         <div className="home container">
-            <h1>Yash</h1>
+            {/* <h1>Yash</h1> */}
             <div className="row">
                 <div className="col text-content">
-                    <h1>Test 1</h1>
+                    <h1>Online Code Editor</h1>
+                    <br/>
+                    <span>A simple online code editor. <br/>Just like Code Pen!</span>
                 </div>
-                <div className="col">
+                <div className="col img-section">
                     <h1>test 2</h1>
+                    <img className="img-size" src={Main} alt="main-img"/>
                 </div>
             </div>
         </div>
